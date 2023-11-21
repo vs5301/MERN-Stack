b57946d8eeca4f09e1fca2c7900094ce6b9701bb
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-11-21 16:16:50 +0530

    Variables

---
+++ JS/variables/script.js
@@ -0,0 +1,40 @@
+// Variables declaration
+
+var age = 20;
+let money = 100;
+const city = "Ludhiana";
+
+// Primitive Data Types
+// string
+let fullName = "John Doe";
+
+// Number
+let accountBalance = 200;
+
+// Boolean
+let isMarries = false;
+
+// Null
+let pendingTransaction = null;
+
+// undefined
+let country = undefined; 
+
+// symbol
+let uniqueID = Symbol("id");
+
+// Non Primitive Data Types
+// Object
+let user = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+}
+
+// Array
+let transactionAmount = [100, 20, 50, 40];
+
+// Function
+let greetUser = () => {
+    console.log("Good Morning");
+}
