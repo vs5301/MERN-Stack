8725d04394658ed64fa4695e68f5d0dbd329fd06
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-11-22 16:20:39 +0530

    JS: Operators

---
+++ JS/operators/script.js
@@ -0,0 +1,120 @@
+// Arithemetic Operators
+
+// Addition operator (+)
+const applesOnHand = 5
+const applesReceieved = 1
+const totalApples = applesOnHand + applesReceieved
+
+// Subtraction operator (-)
+const totalTickets = 100
+const ticketSold = 45
+const ticketsLeft = totalTickets - ticketSold
+
+// Multiplication Operator (*)
+const bookCost = 20
+const booksBought = 15
+const totalCost = bookCost * booksBought
+
+// Division Operator (/)
+const totalPages = 200
+const pagePerDay = 40
+const daysToFinish = totalPages / pagePerDay
+
+// Remainder or Modulo Operator (%)
+const peopleParty = 10
+const pizzasAtParty = 3
+const sliceLeftover = peopleParty % pizzasAtParty
+
+// Increment Operator (++)
+var currentWebsiteViews = 1000
+currentWebsiteViews++
+
+// Decrement Operator (--)
+var basketballPoints = 20
+basketballPoints--
+
+
+// Assignment Operators (=)
+var myTotalApples = 10
+
+// Addition Assignment Operator (**=)
+let population = 100
+population += 50
+
+// Subtraction Assignment Operator (**=)
+var moneyInWallet = 200
+moneyInWallet -= 30
+
+// Multiplication Assignment Operator (**=)
+var cookiesInJar = 12
+cookiesInJar *= 3
+
+// Division Assignment Operator (**=)
+var myTotalPages = 200
+myTotalPages /= 5
+
+// Modulo Assignment Operator (**=)
+var tickets = 25
+tickets %= 4
+
+// Exponent Assignment Operator (**=) : Raise a number to the power of another number
+var base = 2
+base **= 4
+
+
+// Comparison Operators
+
+// Equal to (==) or (===)
+let heightA = "150"
+let heightB = 150
+var results = heightA === heightB
+
+// Not Equal (!=)
+let apples = 5
+let oranges = 7
+var results = apples != oranges
+
+// Not Equal Value or Not Equal Type (!==)
+var result = heightA !== heightB
+
+// Greater Than (>)
+let speedA = 60
+let speedB = 50
+result = speed > speedB
+
+// Lesser Than (<)
+result = speedB < sppedA
+
+// Greater Than or Equal To (>=)
+let weightA = 60
+let weightB = 50
+result = weightA >= weightB
+
+// Less Than or Equal To (<=)
+result = weightA <= weightB
+
+
+// Logical Operators
+
+// Logical AND (&&)
+let isSunny = true
+let isWarm = true
+let shouldGoToPark = isSunny && isWarm
+
+// Logical OR (||)
+let isRaining = false
+let isSnowy = true
+let isNight = false
+result = isRaining || isSnowy || isNight
+
+
+// String Operators (+)
+let greeting = "Hello, "
+let firstName = "Thomas"
+let lastName = 'Tweneboah'
+let message = greeting + firstName + " " + lastName
+
+// += 
+let sentence = "I love"
+sentence += " " + "Javascript"
+let value = "10" + 10
