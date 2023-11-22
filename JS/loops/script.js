4c61029200b34904b177baba8f7048d004234d70
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-11-22 16:38:06 +0530

    JS: Iterations and Loops

---
+++ JS/loops/script.js
@@ -0,0 +1,13 @@
+// FOR Loop
+
+// display 0 - 100 numbers
+for (let index = 1; index <= 100; index++) {
+    console.log(index);
+}
+
+// WHILE Loop
+let i = 99
+while (i > 0) {
+    console.log(i);
+    i--
+}
